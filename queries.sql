9ab4dc8063e28ff410d4bd19cc377772cd9f1863
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-02 17:09:42 +0300

    Start a transaction and set species column to unspecified and roll back to revert the changes

--- queries.sql
+++ queries.sql
@@ -15,3 +15,11 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN TRANSACTION;
+
+UPDATE animals SET species = 'unspecified';
+
+SELECT * FROM animals;
+
+ROLLBACK;
