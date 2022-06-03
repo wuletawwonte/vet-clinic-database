a629e7cfb6863882f5e985ce0ea0a71a0ed73893
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-03 12:52:07 +0300

    Query for animals owned by Melody pond

--- queries.sql
+++ queries.sql
@@ -77,3 +77,4 @@ SELECT neutered, MAX(weight_kg) as Max_weight, MIN(weight_kg) as Max_weight  FRO
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT neutered, AVG(escape_attempts) as Average_escape_attempts FROM animals WHERE date_of_birth <= '2000-12-31' AND date_of_birth >= '1990-01-01' GROUP BY neutered;
 
+SELECT full_name AS owner, name AS animal FROM owners JOIN animals ON owners.id = animals.owner_id WHERE owners.full_name = 'Melody Pond';
